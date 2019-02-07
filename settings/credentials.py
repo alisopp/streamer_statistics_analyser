9f51cef27e00b67bea8f377e60e322c734c1dbc2
Alexander Isopp <alisopp@edu.aau.at>
2019-02-07 20:42:37 +0100

    updated env variables

--- settings/credentials.py
+++ settings/credentials.py
@@ -1,6 +1,11 @@
-# secret client id to be set up with the twitch account
-client_id = ""
+import os
 
-#with open("client_id.txt") as f:
-#    lines = f.readlines();
-#    client_id = lines[0].strip()
+
+def get_client_id():
+    if os.environ.get('TWITCH_CREDENTIALS') is not None:
+        return os.environ['TWITCH_CREDENTIALS']
+    return ""
+
+
+# secret client id to be set up with the twitch account
+client_id = get_client_id()
--- settings/env_variables.py
+++ settings/env_variables.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 savePath = "D:/Miscellioneus/uni/BachelorArbeit/json_data"
@@ -6,6 +7,12 @@ runtimeDataFolder = "C:/Users/AIsop/PycharmProjects/twitch_streamer_statistics_r
 
 # in days
 create_new_statistic_interval = 7
+
+
+def get_debug():
+    if os.environ.get('DEBUG') is not None:
+        return bool(os.environ['DEBUG'])
+    return True
 
 
 def get_www_root():
@@ -23,8 +30,19 @@ def get_url():
 def get_port():
     if os.environ.get('DB_PORT') is not None:
         return int(os.environ['DB_PORT'])
-    return 27018
+    return 27017
 
+
+def get_start_date():
+    if os.environ.get('START_DATE') is not None:
+        result_date = datetime.datetime.strptime(os.environ['START_DATE'], "%d.%m.%Y")
+    else:
+        result_date = datetime.datetime.strptime("06.01.2019","%d.%m.%Y")
+    return result_date + datetime.timedelta(days=(6 - result_date.weekday()))
+
+
+# date from where all website should be generated
+start_date = get_start_date()
 
 db_url = get_url()
 
@@ -33,3 +51,5 @@ db_port = get_port()
 db_name = "stream_reader"
 
 wwwroot = get_www_root()
+
+debug = get_debug()
